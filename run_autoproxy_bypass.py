672ab8a0feba7064a05c1b4356379a4b4481652f
ylasgamers <ylasgamers@gmail.com>
2024-11-30 22:15:14 +0700

    Update run_autoproxy_bypass.py

--- run_autoproxy_bypass.py
+++ run_autoproxy_bypass.py
@@ -22,7 +22,7 @@ RETRIES = 60
 
 DOMAIN_API = {
     "SESSION": "http://api.nodepay.ai/api/auth/session",
-    "PING": "http://52.77.10.116/api/network/ping"
+    "PING": "https://nw.nodepay.org/api/network/ping"
 }
 
 CONNECTION_STATES = {
