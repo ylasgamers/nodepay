1a64fbccf4e0e0aef614b4d44a2c692ba21a745e
ylasgamers <ylasgamers@gmail.com>
2024-11-30 22:15:40 +0700

    Create run_proxy_multi_bypass.py

---
+++ run_proxy_multi_bypass.py
@@ -0,0 +1,241 @@
+import asyncio
+import aiohttp
+import time
+import uuid
+from curl_cffi import requests
+from loguru import logger
+from fake_useragent import UserAgent
+
+def show_warning():
+    confirm = input("By using this tool means you understand the risks. do it at your own risk! \nPress Enter to continue or Ctrl+C to cancel... ")
+
+    if confirm.strip() == "":
+        print("Continuing...")
+    else:
+        print("Exiting...")
+        exit()
+
+# Constants
+PING_INTERVAL = 60
+RETRIES = 60
+
+DOMAIN_API = {
+    "SESSION": "http://api.nodepay.ai/api/auth/session",
+    "PING": "https://nw.nodepay.org/api/network/ping"
+}
+
+CONNECTION_STATES = {
+    "CONNECTED": 1,
+    "DISCONNECTED": 2,
+    "NONE_CONNECTION": 3
+}
+
+status_connect = CONNECTION_STATES["NONE_CONNECTION"]
+browser_id = None
+account_info = {}
+last_ping_time = {}
+
+def uuidv4():
+    return str(uuid.uuid4())
+
+def valid_resp(resp):
+    if not resp or "code" not in resp or resp["code"] < 0:
+        raise ValueError("Invalid response")
+    return resp
+
+async def render_profile_info(proxy, token):
+    global browser_id, account_info
+
+    try:
+        np_session_info = load_session_info(proxy)
+
+        if not np_session_info:
+            # Generate new browser_id
+            browser_id = uuidv4()
+            response = await call_api(DOMAIN_API["SESSION"], {}, proxy, token)
+            valid_resp(response)
+            account_info = response["data"]
+            if account_info.get("uid"):
+                save_session_info(proxy, account_info)
+                await start_ping(proxy, token)
+            else:
+                handle_logout(proxy)
+        else:
+            account_info = np_session_info
+            await start_ping(proxy, token)
+    except Exception as e:
+        logger.error(f"Error in render_profile_info for proxy {proxy}: {e}")
+        error_message = str(e)
+        if any(phrase in error_message for phrase in [
+            "sent 1011 (internal error) keepalive ping timeout; no close frame received",
+            "500 Internal Server Error"
+        ]):
+            logger.info(f"Removing error proxy from the list: {proxy}")
+            remove_proxy_from_list(proxy)
+            return None
+        else:
+            logger.error(f"Connection error: {e}")
+            return proxy
+
+async def call_api(url, data, proxy, token):
+    user_agent = UserAgent(os=['windows', 'macos', 'linux'], browsers='chrome')
+    random_user_agent = user_agent.random
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "User-Agent": random_user_agent,
+        "Content-Type": "application/json",
+        "Origin": "chrome-extension://lgmpfmgeabnnlemejacfljbmonaomfmm",
+        "Accept": "application/json",
+        "Accept-Language": "en-US,en;q=0.5",
+    }
+
+    try:
+        response = requests.post(url, json=data, headers=headers, impersonate="chrome110", proxies={
+                                "http": proxy, "https": proxy}, timeout=30)
+
+        response.raise_for_status()
+        return valid_resp(response.json())
+    except Exception as e:
+        logger.error(f"Error during API call: {e}")
+        raise ValueError(f"Failed API call to {url}")
+
+async def start_ping(proxy, token):
+    try:
+        while True:
+            await ping(proxy, token)
+            await asyncio.sleep(PING_INTERVAL)
+    except asyncio.CancelledError:
+        logger.info(f"Ping task for proxy {proxy} was cancelled")
+    except Exception as e:
+        logger.error(f"Error in start_ping for proxy {proxy}: {e}")
+
+async def ping(proxy, token):
+    global last_ping_time, RETRIES, status_connect
+
+    current_time = time.time()
+
+    if proxy in last_ping_time and (current_time - last_ping_time[proxy]) < PING_INTERVAL:
+        logger.info(f"Skipping ping for proxy { proxy}, not enough time elapsed")
+        return
+
+    last_ping_time[proxy] = current_time
+
+    try:
+        data = {
+            "id": account_info.get("uid"),
+            "browser_id": browser_id,
+            "timestamp": int(time.time()),
+            "version": "2.2.7"
+        }
+
+        response = await call_api(DOMAIN_API["PING"], data, proxy, token)
+        if response["code"] == 0:
+            logger.info(f"Ping successful : {response} with id : {account_info.get('uid')}")
+            RETRIES = 0
+            status_connect = CONNECTION_STATES["CONNECTED"]
+        else:
+            handle_ping_fail(proxy, response)
+    except Exception as e:
+        logger.error(f"Ping failed : {e}")
+        handle_ping_fail(proxy, None)
+
+def handle_ping_fail(proxy, response):
+    global RETRIES, status_connect
+
+    RETRIES += 1
+    if response and response.get("code") == 403:
+        handle_logout(proxy)
+    elif RETRIES < 2:
+        status_connect = CONNECTION_STATES["DISCONNECTED"]
+    else:
+        status_connect = CONNECTION_STATES["DISCONNECTED"]
+
+def handle_logout(proxy):
+    global status_connect, account_info
+
+    status_connect = CONNECTION_STATES["NONE_CONNECTION"]
+    account_info = {}
+    save_status(proxy, None)
+    logger.info(f"Logged out and cleared session info for proxy {proxy}")
+
+def load_proxies(proxy_file):
+    try:
+        with open(proxy_file, 'r') as file:
+            proxies = file.read().splitlines()
+        return proxies
+    except Exception as e:
+        logger.error(f"Failed to load proxies: {e}")
+        raise SystemExit("Exiting due to failure in loading proxies")
+
+def load_tokens(token_file):
+    try:
+        with open(token_file, 'r') as file:
+            tokens = file.read().splitlines()
+        return tokens
+    except Exception as e:
+        logger.error(f"Failed to load tokens: {e}")
+        raise SystemExit("Exiting due to failure in loading tokens")
+
+def save_status(proxy, status):
+    pass
+
+def save_session_info(proxy, data):
+    data_to_save = {
+        "uid": data.get("uid"),
+        "browser_id": browser_id
+    }
+    pass
+
+def load_session_info(proxy):
+    return {}
+
+def is_valid_proxy(proxy):
+    return True
+
+def remove_proxy_from_list(proxy):
+    pass
+
+async def main():
+    all_proxies = load_proxies('local_proxies.txt')  # Load proxies from file
+    all_tokens = load_tokens('token_list.txt')  # Load tokens from token list
+
+    if not all_tokens:
+        print("No tokens found in token_list.txt. Exiting the program.")
+        exit()
+
+    while True:
+        for token in all_tokens:
+            active_proxies = [
+                proxy for proxy in all_proxies if is_valid_proxy(proxy)][:100]
+            tasks = {asyncio.create_task(render_profile_info(
+                proxy, token)): proxy for proxy in active_proxies}
+
+            done, pending = await asyncio.wait(tasks.keys(), return_when=asyncio.FIRST_COMPLETED)
+            for task in done:
+                failed_proxy = tasks[task]
+                if task.result() is None:
+                    logger.info(f"Removing and replacing failed proxy: {failed_proxy}")
+                    active_proxies.remove(failed_proxy)
+                    if all_proxies:
+                        new_proxy = all_proxies.pop(0)
+                        if is_valid_proxy(new_proxy):
+                            active_proxies.append(new_proxy)
+                            new_task = asyncio.create_task(
+                                render_profile_info(new_proxy, token))
+                            tasks[new_task] = new_proxy
+            tasks.pop(task)
+
+            for proxy in set(active_proxies) - set(tasks.values()):
+                new_task = asyncio.create_task(
+                    render_profile_info(proxy, token))
+                tasks[new_task] = proxy
+            await asyncio.sleep(3)
+        await asyncio.sleep(10)
+
+if __name__ == '__main__':
+    show_warning()
+    print("\nAlright, we here! Running the script with tokens from token_list.txt.")
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.info("Program terminated by user.")
