612d6a07fc1b92506d12cf1eaa540cf2c1a8fd73
ylasgamers <ylasgamers@gmail.com>
2024-12-03 03:18:41 +0700

    Update dailyclaim.py

--- dailyclaim.py
+++ dailyclaim.py
@@ -10,7 +10,7 @@ def dailyclaim():
                 url = f"https://api.nodepay.org/api/mission/complete-mission?"
                 headers = {
                     "Authorization": f"Bearer {tokenlist}",
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
                     "Content-Type": "application/json",
                     "Origin": "https://app.nodepay.ai",
                     "Referer": "https://app.nodepay.ai/"
@@ -20,7 +20,7 @@ def dailyclaim():
                     "mission_id":"1"
                 }
 
-                response = requests.post(url, headers=headers, json=data, impersonate="chrome110")
+                response = requests.post(url, headers=headers, json=data, impersonate="chrome131")
                 is_success = response.json().get('success')
                 if is_success == True:
                     print(f'Claim Reward Success!')
