eefa07b1718562a5da190d17024bf8d8cb902ca2
ylasgamers <ylasgamers@gmail.com>
2024-12-02 11:17:35 +0700

    Add files via upload

---
+++ dailyclaim.py
@@ -0,0 +1,33 @@
+from curl_cffi import requests
+
+print(f'Daily Treasure Claim Nodepay Multiple Account') 
+
+def dailyclaim():
+    try:
+        with open('token_list.txt', 'r') as file:
+            local_data = file.read().splitlines()
+            for tokenlist in local_data:
+                url = f"https://api.nodepay.org/api/mission/complete-mission?"
+                headers = {
+                    "Authorization": f"Bearer {tokenlist}",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    "Content-Type": "application/json",
+                    "Origin": "https://app.nodepay.ai",
+                    "Referer": "https://app.nodepay.ai/"
+                }
+                
+                data = {
+                    "mission_id":"1"
+                }
+
+                response = requests.post(url, headers=headers, json=data, impersonate="chrome110")
+                is_success = response.json().get('success')
+                if is_success == True:
+                    print(f'Claim Reward Success!')
+                    print(response.json())
+                else:
+                    print(f'Reward Already Claimed! Or Something Wrong!')
+    except requests.exceptions.RequestException as e:
+        print(f"Error : {e}")
+    
+dailyclaim()
