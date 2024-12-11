86b1e3559f8df7ec4f9261f577eb2694f61a882b
ylasgamers <ylasgamers@gmail.com>
2024-12-11 22:03:37 +0700

    Update run_proxy_multi.py

--- run_proxy_multi.py
+++ run_proxy_multi.py
@@ -21,7 +21,7 @@ RETRIES = 60
 
 DOMAIN_API = {
     "SESSION": "http://api.nodepay.ai/api/auth/session",
-    "PING": "http://18.142.29.174/api/network/ping"
+    "PING": "http://nw.nodepay.ai/api/network/ping"
 }
 
 CONNECTION_STATES = {
